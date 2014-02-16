a61ecdf53deffe71cc4265b4bc041f9e75832b3a
刘心贤（jack） <liuxin201234@gmail.com>
2014-02-16 11:18:42 +0800

    commit 3

---
+++ sf/src/com/sina/weibo/sdk/codestyle/GetData.java
@@ -0,0 +1,11 @@
+package com.sina.weibo.sdk.codestyle;
+
+import java.util.Date;
+
+public class GetData {
+	public static void main(String[] args) {
+		System.out.println("-=---------------");
+		Date date = new Date(1396249432);
+		System.out.println(date.getYear());
+	}
+}
